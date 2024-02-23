754c25c5e506a0e265343f69b1a7ff137cdfb952
Anastasia <rotaru.nastia@gmail.com>
2024-02-23 21:30:41 +0200

    added images

---
+++ HW_Games/categories.js
@@ -0,0 +1,76 @@
+const kids = {
+    games: [
+        {
+            name: "Tetris",
+            img: "image here"
+        },
+        {
+            name: "Angry birds",
+            img: "image here"
+        },
+        {
+            name: "Mario",
+            img: "image here"
+        },
+    ]
+};
+
+const teens = {
+    games: [
+        {
+            name: "Stray",
+            img: "image here"
+        },
+        {
+            name: "Call of duty",
+            img: "image here"
+        },
+        {
+            name: "Detroit become human",
+            img: "image here"
+        },
+    ]
+}
+
+const youngAdults = {
+    games: [
+        {
+            name: "Little nightmares",
+            img: "image here"
+        },
+        {
+            name: "God of war",
+            img: "image here"
+        },
+        {
+            name: "Dead space",
+            img: "image here"
+        },
+    ]
+}
+
+const adults = {
+    games: [
+        {
+            name: "Resident evil village",
+            img: "image here"
+        },
+        {
+            name: "The witcher",
+            img: "image here"
+        },
+        {
+            name: "Batman: Arkham Knight",
+            img: "image here"
+        },
+    ]
+}
+
+const categories = {
+    kids,
+    teens,
+    youngAdults,
+    adults
+}
+
+module.export = categories;
--- HW_Games/index.js
+++ HW_Games/index.js
@@ -1,38 +1,128 @@
-const button = document.querySelector('#enter');
+const kids = {
+    games: [
+        {
+            name: "Tetris",
+            img: "Tetris"
+        },
+        {
+            name: "Angry birds",
+            img: "angryBirds"
+        },
+        {
+            name: "Mario",
+            img: "Mario"
+        },
+    ]
+};
+
+const teens = {
+    games: [
+        {
+            name: "Stray",
+            img: "Stray"
+        },
+        {
+            name: "Call of duty",
+            img: "CallOfDuty"
+        },
+        {
+            name: "Detroit become human",
+            img: "Detroit"
+        },
+    ]
+}
+
+const youngAdults = {
+    games: [
+        {
+            name: "Little nightmares",
+            img: "Nightmares"
+        },
+        {
+            name: "God of war",
+            img: "GodOfWar"
+        },
+        {
+            name: "Dead space",
+            img: "DeadSpace"
+        },
+    ]
+}
+
+const adults = {
+    games: [
+        {
+            name: "Resident evil village",
+            img: "ResidentEvil"
+        },
+        {
+            name: "The Witcher",
+            img: "TheWitcher"
+        },
+        {
+            name: "Batman: Arkham Knight",
+            img: "BecauseIAmBatman"
+        },
+    ]
+}
+
+const categories = {
+    kids,
+    teens,
+    youngAdults,
+    adults
+}
+
+
+const btnEnter = document.querySelector('#enter');
 const inputAge = document.querySelector('#age');
-const divView = document.querySelector('#view')
-const games = ["Tetris", "Angry birds", "Mario"]
-const games2 = ["Tetris2", "Angry birds2", "Mario2"];
-const games3 = ["Tetris3", "Angry birds3", "Mario3"];
+const divView = document.querySelector('.accordion-area');
 
 function viewGames(games) {
     let viewGames = '';
 
-    // for (i = 0; i < games.length, i++) {
-    //     viewGames += `<div class="game">${games[i]}</div>`;
-    // }
-
-    // games.forEach(game => {
-    //     viewGames += `<div class="game">${game}</div>`;
-    // });
-
-    // viewGames = games.map(game => `<div class="game">${game}</div>`).join();
-
     for (const game of games) {
-        viewGames += `<div class="game">${game}</div>`;
+        viewGames += `<button class="accordion">${game.name}</button>`;
+        viewGames += `<div class="panel"><img src='./images/${game.img}.jpg' alt=${game.name}/></div>`;
     }
 
     divView.innerHTML = viewGames;
+
+    setAccordionEvents();
+}
+
+function setAccordionEvents() {
+    const btnAccordions = document.querySelectorAll('.accordion');
+
+    function accordionAction() {
+        this.classList.toggle("active");
+
+        const panel = this.nextElementSibling;
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+
+    for (const btn of btnAccordions) {
+        btn.addEventListener("click", accordionAction)
+    }
 }
 
 function ageGame() {
     const age = inputAge.value;
 
     switch (true) {
-        case age >= 1 && age <= 10: viewGames(games); break;
-        case age >= 11 && age <= 15: viewGames(games2); break;
-        case age >= 15: viewGames(games3); break;
+        case age >= 1 && age <= 10: viewGames(categories.kids.games); break;
+        case age >= 11 && age <= 16: viewGames(categories.teens.games); break;
+        case age >= 17 && age <= 18: viewGames(categories.youngAdults.games); break;
+        case age > 18: viewGames(categories.adults.games); break;
     }
 }
 
-button.addEventListener("click", ageGame)
+btnEnter.addEventListener("click", ageGame)
+
+
+
